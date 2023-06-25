4467f02efb8c11f151c195ccbff78b2417251fb0
Iva-Petrovic <ivapet98@gmail.com>
2023-06-25 17:34:26 +0200

    Kreirana pocetna struktura za NewArrival.jsx

--- reactfront/src/pages/HomePage.jsx
+++ reactfront/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import FeaturedProducts from '../components/home/FeaturedProducts';
 import Categories from '../components/home/Categories';
 import Collection from '../components/home/Collection';
+import NewArrival from '../components/home/NewArrival';
 
 export class HomePage extends Component {
   render() {
@@ -11,6 +12,7 @@ export class HomePage extends Component {
             <FeaturedProducts/>
             <Collection/>
             <Categories/>
+            <NewArrival/>
 
         </Fragment>
     </div>;
