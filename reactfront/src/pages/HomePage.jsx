6cd3f370f78df9e4a81a852e59361477c138ce54
Iva-Petrovic <ivapet98@gmail.com>
2023-06-25 17:50:15 +0200

    Dodat dizajn za HomeTop.jsx

--- reactfront/src/pages/HomePage.jsx
+++ reactfront/src/pages/HomePage.jsx
@@ -3,12 +3,13 @@ import FeaturedProducts from '../components/home/FeaturedProducts';
 import Categories from '../components/home/Categories';
 import Collection from '../components/home/Collection';
 import NewArrival from '../components/home/NewArrival';
-
+import HomeTop from '../components/home/HomeTop';
 export class HomePage extends Component {
   render() {
     return <div>
         <Fragment>
 
+            <HomeTop/>
             <FeaturedProducts/>
             <Collection/>
             <Categories/>
