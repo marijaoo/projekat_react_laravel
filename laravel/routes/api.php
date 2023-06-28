9a3fb6c57c21b2ef7b628931d2cdcd43c45989de
marijaoo <ocelmarija@gmail.com>
2023-06-28 12:18:12 +0200

    Categories komponenta

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\SiteInfoController;
+use App\Http\Controllers\Admin\CategoryController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -12,3 +13,4 @@ use App\Http\Controllers\Admin\SiteInfoController;
 Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
 Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
 Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']);
+Route::get('/allcategory', [CategoryController::class, 'AllCategory']);
--- reactfront/src/components/home/HomeTop.jsx
+++ reactfront/src/components/home/HomeTop.jsx
@@ -1,20 +1,39 @@
 import React, { Component, Fragment } from 'react'
-import {Container,Row,Col,Card} from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
+//import Slider from './Slider'
 import HomeSlider from './HomeSlider'
 import MegaMenu from './MegaMenu'
+import AppURL from '../../api/AppURL';
+import axios from 'axios'
 
 class HomeTop extends Component {
+
+     constructor() {
+          super();
+          this.state = {
+               MenuData: []
+          }
+     }
+
+     componentDidMount() {
+          axios.get(AppURL.AllCategoryDetails).then(response => {
+               this.setState({ MenuData: response.data });
+
+          }).catch(error => {
+
+          });
+     }
      render() {
           return (
                <Fragment>
-        <Container className="p-0 m-0 overflow-hidden" fluid={true}>
+                    <Container className="p-0 m-0 overflow-hidden" fluid={true}>
                          <Row>
                               <Col lg={3} md={3} sm={12}>
-                              <MegaMenu />
+                                   <MegaMenu data={this.state.MenuData} />
                               </Col>
 
                               <Col lg={9} md={9} sm={12}>
-                                <HomeSlider />
+                                   <HomeSlider />
                               </Col>
                          </Row>
                     </Container>
