a58d1e7b3a241e4d594f741faa9200cbdb87c5c6
marijaoo <ocelmarija@gmail.com>
2023-06-28 15:57:43 +0200

    ProductCategory komponenta

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductListController;
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -14,3 +15,6 @@ Route::get('/getvisitor', [VisitorController::class, 'GetVisitorDetails']);
 Route::post('/postcontact', [ContactController::class, 'PostContactDetails']);
 Route::get('/allsiteinfo', [SiteInfoController::class, 'AllSiteInfo']);
 Route::get('/allcategory', [CategoryController::class, 'AllCategory']);
+Route::get('/productlistbyremark/{remark}', [ProductListController::class, 'ProductListByRemark']);
+Route::get('/productlistbycategory/{category}', [ProductListController::class, 'ProductListByCategory']);
+Route::get('/productlistbysubcategory/{category}/{subcategory}', [ProductListController::class, 'ProductListBySubcategory']);
